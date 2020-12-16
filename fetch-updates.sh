9f0f389acdca3de88a7f19f1a2aeef40ca00632c
Nicolas Mattiocco <nicolas.mattiocco@gmail.com>
2020-12-16 09:59:42 +0100

    data-update-2020-12-16 09:59:41

--- fetch-updates.sh
+++ fetch-updates.sh
@@ -4,8 +4,20 @@ display_usage() {
 	echo -e "\nUsage: $0 \n"
 }
 
+start_time="$(date -u +%s)"
+current_date=$(python -c 'from datetime import datetime as dt; print(dt.today().strftime("%Y-%m-%d"))')
+current_datetime=$(python -c 'from datetime import datetime as dt; print(dt.today().strftime("%Y-%m-%d %H:%M:%S"))')
 
 # python CWE/fetch-and-update.py
 # python CPE/fetch-and-update.py
 # python CVE/fetch-and-update.py
-python VIA/fetch-and-update.py
+# python VIA/fetch-and-update.py
+
+echo "${current_datetime}" > lastupdate.txt
+
+git add .
+git commit -m "data-update-${current_datetime}"
+
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "Total of $elapsed seconds elapsed"
