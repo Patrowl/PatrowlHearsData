f9f04ef88543eff5e8d7d35b5a327b46313546fe
MaKyOtOx <nicolas.mattiocco@gmail.com>
2020-12-17 13:13:24 +0100

    data-update-2020-12-17 13:07:45

--- fetch-updates.sh
+++ fetch-updates.sh
@@ -4,22 +4,60 @@ display_usage() {
 	echo -e "\nUsage: $0 \n"
 }
 
+DO_DATA_UPDATE=1
+DO_PUSH=1
+
+while (( "$#" )); do
+  case "$1" in
+    -n|--no-data-update)
+      DO_DATA_UPDATE=0
+      shift
+      ;;
+    -l|--no-data-push)
+      DO_PUSH=0
+      shift
+      ;;
+    # -b|--my-flag-with-argument)
+    #   if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+    #     MY_FLAG_ARG=$2
+    #     shift 2
+    #   else
+    #     echo "Error: Argument for $1 is missing" >&2
+    #     exit 1
+    #   fi
+    #   ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      display_usage
+      exit 1
+      ;;
+  esac
+done
+
 start_time="$(date -u +%s)"
 current_date=$(python -c 'from datetime import datetime as dt; print(dt.today().strftime("%Y-%m-%d"))')
 current_datetime=$(python -c 'from datetime import datetime as dt; print(dt.today().strftime("%Y-%m-%d %H:%M:%S"))')
 
-# python CWE/fetch-and-update.py
-# python CPE/fetch-and-update.py
-# python CVE/fetch-and-update.py
-# python VIA/fetch-and-update.py
+
+[ $DO_DATA_UPDATE -eq 1 ] && {
+	python CWE/fetch-and-update.py
+	python CPE/fetch-and-update.py
+	python CVE/fetch-and-update.py
+	python VIA/fetch-and-update.py
+}
 
 echo "${current_datetime}" > lastupdate.txt
 
-git add .
-git commit -m "data-update-${current_datetime}"
-git push origin :refs/tags/$current_date
-git tag -f $current_date
-git push && git push --tags
+[ $DO_PUSH -eq 1 ] && {
+	git add .
+	git commit -m "data-update-${current_datetime}"
+	git push origin :refs/tags/$current_date
+	git tag -f $current_date
+	git push && git push --tags
+}
 
 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
