34eaf2504b4f3b5dabd2a63266df85d4fb895f87
MaKyOtOx <nicolas.mattiocco@gmail.com>
2020-12-21 17:51:53 +0100

    data-update-2020-12-21 17:48:50

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python -mplatform | grep -qi Ubuntu && {
+  sudo apt-get install -y python3 python3-pip git
+  pip3 install virtualenv
+}
+python -mplatform | grep -qi centos && {
+  sudo yum install -y git python3 python3-pip python3-virtualenv
+}
+python -mplatform | grep -qi macOS && {
+  brew update
+  brew install python3 git
+  python -m ensurepip
+  pip install virtualenv
+}
+
+virtualenv env --python=python3
+env/bin/pip install -r requirements.txt
