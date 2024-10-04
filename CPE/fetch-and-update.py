700c36bff3a4d565c78fb78d9c9082d6694c725a
Pascal SUN <35567930+pascal-sun@users.noreply.github.com>
2024-10-04 17:24:21 +0200

    style: ruff format #ARS-559

--- CPE/fetch-and-update.py
+++ CPE/fetch-and-update.py
@@ -8,18 +8,18 @@ import xml.etree.ElementTree as ET
 from tqdm import tqdm
 
 
-CPE_FILENAME_ZIP = 'official-cpe-dictionary_v2.3.xml.zip'
-CPEMATCH_FILENAME_ZIP = 'nvdcpematch-1.0.json.zip'
+CPE_FILENAME_ZIP = "official-cpe-dictionary_v2.3.xml.zip"
+CPEMATCH_FILENAME_ZIP = "nvdcpematch-1.0.json.zip"
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
-if not os.path.exists(BASEDIR+'/data/'):
-    os.makedirs(BASEDIR+'/data/')
-if not os.path.exists(BASEDIR+'/nvd/'):
-    os.makedirs(BASEDIR+'/nvd/')
+if not os.path.exists(BASEDIR + "/data/"):
+    os.makedirs(BASEDIR + "/data/")
+if not os.path.exists(BASEDIR + "/nvd/"):
+    os.makedirs(BASEDIR + "/nvd/")
 
 cpes = {}
-counters = {'cpes': 0, 'vendors': 0, 'products': 0}
-counters_diff = {'cpes': 0}
+counters = {"cpes": 0, "vendors": 0, "products": 0}
+counters_diff = {"cpes": 0}
 
 # print("[+] Backup old files")
 # try:
@@ -29,9 +29,11 @@ counters_diff = {'cpes': 0}
 #     has_backup = False
 
 print("[+] Downloading CPE dictionary from NVD")
-r = requests.get('https://nvd.nist.gov/feeds/xml/cpe/dictionary/'+CPE_FILENAME_ZIP, stream=True)
-with open(BASEDIR+"/nvd/"+CPE_FILENAME_ZIP, 'wb') as f:
-    pbar = tqdm(unit="B", unit_scale=True, total=int(r.headers['Content-Length']))
+r = requests.get(
+    "https://nvd.nist.gov/feeds/xml/cpe/dictionary/" + CPE_FILENAME_ZIP, stream=True
+)
+with open(BASEDIR + "/nvd/" + CPE_FILENAME_ZIP, "wb") as f:
+    pbar = tqdm(unit="B", unit_scale=True, total=int(r.headers["Content-Length"]))
     for chunk in r.iter_content(chunk_size=1024):
         f.write(chunk)
         pbar.update(1024)
@@ -39,25 +41,27 @@ with open(BASEDIR+"/nvd/"+CPE_FILENAME_ZIP, 'wb') as f:
 
 # Process Official CPE dictionnary
 print("[+] Processing CPE dictionary from NVD")
-z = zipfile.ZipFile(BASEDIR+"/nvd/"+CPE_FILENAME_ZIP, 'r')
+z = zipfile.ZipFile(BASEDIR + "/nvd/" + CPE_FILENAME_ZIP, "r")
 xmlfile = z.open(z.namelist()[0])
 root = ET.fromstring(xmlfile.read())
 
-for xcpe in tqdm(root.findall('{http://cpe.mitre.org/dictionary/2.0}cpe-item')):
-    cpe_vector = xcpe.find('{http://scap.nist.gov/schema/cpe-extension/2.3}cpe23-item').attrib['name']
-    cpe_title = xcpe.find('{http://cpe.mitre.org/dictionary/2.0}title').text
-    cpe_vendor = cpe_vector.split(':')[3]
-    cpe_product = cpe_vector.split(':')[4]
+for xcpe in tqdm(root.findall("{http://cpe.mitre.org/dictionary/2.0}cpe-item")):
+    cpe_vector = xcpe.find(
+        "{http://scap.nist.gov/schema/cpe-extension/2.3}cpe23-item"
+    ).attrib["name"]
+    cpe_title = xcpe.find("{http://cpe.mitre.org/dictionary/2.0}title").text
+    cpe_vendor = cpe_vector.split(":")[3]
+    cpe_product = cpe_vector.split(":")[4]
 
     if cpe_vendor not in cpes.keys():
         cpes.update({cpe_vendor: {}})
-        counters.update({'vendors': counters['vendors']+1})
+        counters.update({"vendors": counters["vendors"] + 1})
     if cpe_product not in cpes[cpe_vendor].keys():
         cpes[cpe_vendor].update({cpe_product: {}})
-        counters.update({'products': counters['products']+1})
+        counters.update({"products": counters["products"] + 1})
     if cpe_vector not in cpes[cpe_vendor][cpe_product].keys():
         cpes[cpe_vendor][cpe_product].update({cpe_vector: cpe_title})
-        counters.update({'cpes': counters['cpes']+1})
+        counters.update({"cpes": counters["cpes"] + 1})
 
 # Quickfix: comment CPE Match Feed data, because:
 # - as JSON is very large and kills the process with `json.loads` (we should use pandas to process the data),
@@ -103,8 +107,8 @@ for xcpe in tqdm(root.findall('{http://cpe.mitre.org/dictionary/2.0}cpe-item')):
 #     zf.write(BASEDIR+'/data/cpes.json', arcname='cpes.json')
 
 print("[+] Building diff file from latest CPE references")
-with open(BASEDIR+'/data/cpes-base.json', "r") as jfo:
-    cpes_oldies = json.loads(jfo.read())['cpes']
+with open(BASEDIR + "/data/cpes-base.json", "r") as jfo:
+    cpes_oldies = json.loads(jfo.read())["cpes"]
     cpes_diffs = {}
 
     # Loop over new vendors list
@@ -122,19 +126,15 @@ with open(BASEDIR+'/data/cpes-base.json', "r") as jfo:
                         **cpes_diffs,
                         **{
                             n_vendor: {
-                                n_product: {
-                                    n_cpe: cpes[n_vendor][n_product][n_cpe]
-                                }
+                                n_product: {n_cpe: cpes[n_vendor][n_product][n_cpe]}
                             }
-                        }
+                        },
                     }
-                    counters_diff.update({'cpes': counters_diff['cpes']+1})
+                    counters_diff.update({"cpes": counters_diff["cpes"] + 1})
 
     print(counters_diff)
 
-    with open(BASEDIR+'/data/cpes-diff.json', "w") as jf:
-        jf.write(json.dumps({
-            'cpes': cpes_diffs
-        }))
+    with open(BASEDIR + "/data/cpes-diff.json", "w") as jf:
+        jf.write(json.dumps({"cpes": cpes_diffs}))
     # with zipfile.ZipFile(BASEDIR+'/data/cpes-diff.json.zip', 'w', zipfile.ZIP_DEFLATED) as zf:
     #     zf.write(BASEDIR+'/data/cpes-diff.json', arcname='cpes-diff.json')
